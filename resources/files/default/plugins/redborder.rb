d79b43a075eec4d848850f0049f8d9ecd48feae2
Luis Blanco <ljblanco@redborder.com>
2023-02-15 16:12:17 +0000

    machine identification opt

--- resources/files/default/plugins/redborder.rb
+++ resources/files/default/plugins/redborder.rb
@@ -3,23 +3,15 @@ provides 'redborder'
 redborder Mash.new
 redborder[:rpms] = Mash.new
 redborder[:is_sensor]=false
-redborder[:is_manager]=false
-redborder[:is_proxy]=false
+rpms = `rpm -qa`
+%w(manager proxy ips).map { |m_type| redborder[("is_"+m_type).to_sym] = rpms.include?("redborder-" + m_type) }
 
-rpmsout=`rpm -qa | grep redborder-ips`
-if $?.success? 
+if redborder.is_ips
     redborder[:is_sensor]=true
     redborder[:snort]= Mash.new
     redborder[:snort][:version] = `snort --version 2>&1|grep Version|sed 's/.*Version //' | sed 's/ .*//'|awk '{printf("%s", $1)}'`
     redborder[:barnyard2]= Mash.new
     redborder[:barnyard2][:version] = `barnyard2 --version 2>&1|grep -i Version|sed 's/.*Version //'| sed 's/ .*//'|awk '{printf("%s", $1)}'`
-else
-    rpmsout=`rpm -qa | grep redborder-proxy`
-    if $?.success?
-      redborder[:is_proxy]=true
-    else
-      redborder[:is_manager]=true
-    end
 end
 
 redborder[:dmidecode] = Mash.new
